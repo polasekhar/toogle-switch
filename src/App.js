e34129a6ef2a81cad46e5ecdc29df096b39c4085
polasekhar <chandupola28@gmail.com>
2021-03-17 15:07:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css';
+import Switch from './Switch';
+
+function App() {
+  const [value, setValue] = useState(false);
+  return (
+    <div className="app">
+      <Switch isOn={value} onColor="#EF476F" handleToggle={() => setValue(!value)} />
+    </div>
+  );
+}
+
+export default App;
